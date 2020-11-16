5acbaa456718bb66a08973f4362368f1be757a04
AlistarIsCow <62110503+AlistarIsCow@users.noreply.github.com>
2020-11-16 16:40:43 +0100

    Generator v2

--- generator.py
+++ generator.py
@@ -1,6 +1,7 @@
-def Generator(n=100):
+def Generator(n=100, p=20):
     import random
-    output = "{} \n".format(n)
+    output = "{} \n".format(p)
+    output += "{} \n".format(n)
     for i in range(n-1):
         output += "{} \n".format(int(random.uniform(1,n*10)))
     output += "{}".format(int(random.uniform(1, n * 10)))
