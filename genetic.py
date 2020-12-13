c3da94ed955a7dd8e38369716ef5765781fbde58
ms2jpg <28683657+ms2jpg@users.noreply.github.com>
2020-12-14 00:47:48 +0100

    optymalizacje

--- genetic.py
+++ genetic.py
@@ -34,7 +34,6 @@ class Individual:
 
     def random(self):
         for task in self.tasks:
-            xd = task.processorID
             task.processorID = random.randint(0, self.processors-1)
 
     def getProcessors(self):
@@ -81,20 +80,25 @@ class Population:
     config = {}
     def __init__(self, config, processors = 0, tasks = []):
         self.config = config
+        self.tasks = tasks
         for i in range(self.config['population_size']):
             self.population.append(Individual(processors, tasks))
         # self.population[0].greedy()
 
     def crossover(self, _a, _b):
         cutoff = int(len(_a.tasks) * random.random())
-        a = copy.deepcopy(_a)
-        b = copy.deepcopy(_b)
+        # a = copy.deepcopy(_a)
+        # b = copy.deepcopy(_b)
+        c = Individual(_a.processors, self.tasks)
+        for i in range(0, cutoff + 1):
+            c.tasks[i].processorID = _b.tasks[i].processorID
         # print([a.tasks[:cutoff], b.tasks[:cutoff]])
-        a.tasks[:cutoff], b.tasks[:cutoff] = b.tasks[:cutoff], a.tasks[:cutoff]
-        return a
+        # a.tasks[:cutoff], b.tasks[:cutoff] = b.tasks[:cutoff], a.tasks[:cutoff]
+        return c
 
     def run(self):
         size = self.config['population_size']
+        lastFitness = 0
         for generation in range(self.config['iterations']):
             while len(self.population) < 2 * size:
                 child = self.crossover(
@@ -107,7 +111,10 @@ class Population:
             self.population.sort(key=lambda individual: individual.fitness())
             while len(self.population) > size:
                 self.population.pop()
-            print(f'Generacja {generation}, fitness {self.population[0].fitness()}')
+            bestFitness = self.population[0].fitness()
+            if bestFitness != lastFitness:
+                lastFitness = bestFitness
+                print(f'Generacja {generation}, fitness {self.population[0].fitness()}')
 
 population = Population(config, 25, [198, 157, 462, 779, 6, 98, 316, 450, 901, 372, 941, 94, 366, 781, 23, 16, 200, 686, 45, 311, 744, 784, 842, 168, 467, 214, 547, 74, 14, 499, 283, 981, 822, 621, 140, 895, 364, 185, 128, 794, 18, 646, 260, 419, 751, 532, 743, 923, 490, 478, 527, 543, 60, 915, 359, 90, 28, 44, 907, 18, 484, 718, 17, 820, 592, 11, 431, 657, 428, 373, 3, 58, 618, 7, 60, 980, 508, 813, 15, 568, 832, 625, 300, 2, 29, 6, 11, 8, 198, 701, 150, 500, 480, 478, 878, 380, 193, 367, 971, 276, 846, 590, 923, 961, 777, 241, 16, 91, 6, 419, 403, 456, 982, 41, 557])
 population.run()
